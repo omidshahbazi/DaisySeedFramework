e20945719591a4a755e33d38edf97a5202fd49a2
Omid Shahbazi <sh.omid.m@gmail.com>
2024-04-16 09:34:03 +0330

    Modified   DSP Added      Common.h Added      DaisySeedHAL.h

---
+++ Common.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "DSP/Common.h"
+
+enum class GPIOPins
+{
+	Pin0 = 0, // Digital
+	Pin1,	  // Digital
+	Pin2,	  // Digital
+	Pin3,	  // Digital
+	Pin4,	  // Digital
+	Pin5,	  // Digital
+	Pin6,	  // Digital
+	Pin7,	  // Digital
+	Pin8,	  // Digital
+	Pin9,	  // Digital
+	Pin10,	  // Digital
+	Pin11,	  // Digital
+	Pin12,	  // Digital
+	Pin13,	  // Digital
+	Pin14,	  // Digital
+	Pin15,	  // Analog/Digital
+	Pin16,	  // Analog/Digital
+	Pin17,	  // Analog/Digital
+	Pin18,	  // Analog/Digital
+	Pin19,	  // Analog/Digital
+	Pin20,	  // Analog/Digital
+	Pin21,	  // Analog/Digital
+	Pin22,	  // Analog/Digital
+	Pin23,	  // Analog/Digital
+	Pin24,	  // Analog/Digital
+	Pin25,	  // Analog/Digital
+	Pin26,	  // Digital
+	Pin27,	  // Digital
+	Pin28,	  // Analog/Digital
+	Pin29,	  // Digital
+	Pin30,	  // Digital
+	COUNT
+};
---
+++ DaisySeedHAL.h
@@ -0,0 +1,259 @@
+#pragma once
+#ifndef DAISY_SEED_HAL_H
+#define DAISY_SEED_HAL_H
+
+#include "DSP/IHAL.h"
+#include "Common.h"
+#include "DSP/Debug.h"
+#include <daisy_seed.h>
+
+using namespace daisy;
+
+// const int PWM_FREQUENCY = 240;
+// const int PWM_CHANNEL = 1;
+
+class DaisySeedHAL : public IHAL
+{
+	// private:
+	// 	struct PWMChannel
+	// 	{
+	// 	public:
+	// 		bool Used;
+	// 		uint8 Channel;
+	// 		uint8 Pin;
+	// 	};
+
+public:
+	DaisySeedHAL(void)
+		: m_PWMResolution(0),
+		  m_PWMMaxDutyCycle(0),
+		  m_AnalogReadResolution(0),
+		  m_MaxAnalogValue(0)
+	{
+		// uint8 channelIndex = 0;
+		// for (auto &pwmChannel : m_PWMChannels)
+		// 	pwmChannel = {false, channelIndex++, 0};
+
+		SetAnalogReadResolution(12);
+		SetPWMResolution(16);
+	}
+
+	void *Allocate(uint16 Size) override
+	{
+		return malloc(Size);
+	}
+
+	void Deallocate(void *Memory) override
+	{
+		free(Memory);
+	}
+
+	bool IsAnAnaloglPin(uint8 Pin) const override
+	{
+		switch (Pin)
+		{
+		case (uint8)GPIOPins::Pin15:
+		case (uint8)GPIOPins::Pin16:
+		case (uint8)GPIOPins::Pin17:
+		case (uint8)GPIOPins::Pin18:
+		case (uint8)GPIOPins::Pin19:
+		case (uint8)GPIOPins::Pin20:
+		case (uint8)GPIOPins::Pin21:
+		case (uint8)GPIOPins::Pin22:
+		case (uint8)GPIOPins::Pin23:
+		case (uint8)GPIOPins::Pin24:
+		case (uint8)GPIOPins::Pin25:
+		case (uint8)GPIOPins::Pin28:
+			return true;
+
+		default:
+			return false;
+		}
+	}
+
+	bool IsADigitalPin(uint8 Pin) const override
+	{
+		return true;
+	}
+
+	bool IsAnInputPin(uint8 Pin) const override
+	{
+		return true;
+	}
+
+	bool IsAnOutputPin(uint8 Pin) const override
+	{
+		return true;
+	}
+
+	bool IsAPWMPin(uint8 Pin) const override
+	{
+		return true;
+	}
+
+	void SetPWMResolution(uint8 Value) override
+	{
+		ASSERT(8 <= Value && Value <= 16, "Invalid Value");
+
+		m_PWMResolution = Value;
+		m_PWMMaxDutyCycle = (1 << m_PWMResolution) - 1;
+	}
+
+	uint8 GetPWMResolution(void) const override
+	{
+		return m_PWMResolution;
+	}
+
+	void SetPinMode(uint8 Pin, PinModes Mode) override
+	{
+		ASSERT(Mode != PinModes::Input || IsAnInputPin(Pin), "Pin %i is not an input pin", Pin);
+		ASSERT(Mode != PinModes::Output || IsAnOutputPin(Pin), "Pin %i is not an output pin", Pin);
+		ASSERT(Mode != PinModes::PWM || IsAPWMPin(Pin), "Pin %i is not an PWM pin", Pin);
+
+		// switch (Mode)
+		// {
+		// case PinModes::Input:
+		// 	pinMode(Pin, INPUT);
+		// 	break;
+
+		// case PinModes::Output:
+		// 	pinMode(Pin, OUTPUT);
+		// 	break;
+
+		// case PinModes::PWM:
+		// 	InitializePWM(Pin);
+		// 	break;
+		// }
+	}
+
+	void SetAnalogReadResolution(uint8 Value) override
+	{
+		ASSERT(8 <= Value && Value <= 16, "Invalid Value");
+
+		m_AnalogReadResolution = Value;
+		m_MaxAnalogValue = 1 << Value;
+
+		// analogReadResolution(m_AnalogReadResolution);
+	}
+
+	uint8 GetAnalogReadResolution(void) const override
+	{
+		return m_AnalogReadResolution;
+	}
+
+	float AnalogRead(uint8 Pin) const override
+	{
+		// return (analogRead(Pin) / (float)m_MaxAnalogValue);
+	}
+
+	void AnalogWrite(uint8 Pin, float Value) override
+	{
+		ASSERT(0 <= Value && Value <= 1, "Invalid Value");
+
+		// analogWrite(Pin, Value * m_MaxAnalogValue);
+	}
+
+	bool DigitalRead(uint8 Pin) const override
+	{
+		// return (digitalRead((uint8)Pin) != LOW);
+	}
+
+	void DigitalWrite(uint8 Pin, bool Value) override
+	{
+		// digitalWrite(Pin, Value);
+	}
+
+	void PWMWrite(uint8 Pin, float Value) override
+	{
+		ASSERT(0 <= Value && Value <= 1, "Invalid Value");
+
+		// PWMChannel *channel = nullptr, *it = m_PWMChannels;
+		// for (auto &pwmChannel : m_PWMChannels)
+		// {
+		// 	if (!pwmChannel.Used || pwmChannel.Pin != Pin)
+		// 	{
+		// 		++it;
+
+		// 		continue;
+		// 	}
+
+		// 	channel = it;
+
+		// 	break;
+		// }
+
+		// ASSERT(channel != nullptr, "Couldn't find the channel attached to the Pin %i", Pin);
+
+		// ledcWrite(channel->Channel, Value * m_PWMMaxDutyCycle);
+	}
+
+	void Print(const char *Value) const
+	{
+		daisy::DaisySeed::Print(Value);
+	}
+
+	void Break(void) const override
+	{
+		// daisy::DaisySeed::
+	}
+
+	void Delay(uint16 Ms) const override
+	{
+		System::Delay(Ms);
+	}
+
+private:
+	void InitializePWM(uint8 Pin)
+	{
+		// PWMChannel *channel = nullptr, *it = m_PWMChannels;
+		// for (auto &pwmChannel : m_PWMChannels)
+		// {
+		// 	if (!pwmChannel.Used || pwmChannel.Pin != Pin)
+		// 	{
+		// 		++it;
+
+		// 		continue;
+		// 	}
+
+		// 	channel = it;
+
+		// 	break;
+		// }
+
+		// if (channel == nullptr)
+		// {
+		// 	it = m_PWMChannels;
+		// 	for (auto &pwmChannel : m_PWMChannels)
+		// 	{
+		// 		if (pwmChannel.Used)
+		// 		{
+		// 			++it;
+
+		// 			continue;
+		// 		}
+
+		// 		channel = it;
+
+		// 		break;
+		// 	}
+
+		// 	ASSERT(channel != nullptr, "Out of PWM channel");
+		// }
+
+		// channel->Pin = Pin;
+		// channel->Used = true;
+
+		// ledcSetup(channel->Channel, PWM_FREQUENCY, m_PWMResolution);
+
+		// ledcAttachPin(Pin, channel->Channel);
+	}
+
+private:
+	// PWMChannel m_PWMChannels[SOC_LEDC_CHANNEL_NUM];
+	uint8 m_PWMResolution;
+	uint32 m_PWMMaxDutyCycle;
+	uint32 m_AnalogReadResolution;
+	uint32 m_MaxAnalogValue;
+};
+
+#endif
